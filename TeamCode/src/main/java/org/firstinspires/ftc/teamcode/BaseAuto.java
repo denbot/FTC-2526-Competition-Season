5d00b4237e099648a3bb8cc08398634b8e97384f
Xio <xiomara@den.bot>
2026-01-03 09:51:08 -0700

    made new auto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BaseAuto.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+@Autonomous(name = "BaseAuto")
+public class BaseAuto extends OpMode {
+    @Override
+    public void init(){
+
+    }
+    @Override
+    public void loop(){
+
+    }
+}
